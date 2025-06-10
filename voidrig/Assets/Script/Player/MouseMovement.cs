2ad47be61d9070d66418b826d42a621c72489594
ThemBulolqua <102475553+chuyennguyenn@users.noreply.github.com>
2025-06-09 23:49:56 -0500

    moving + jumping

---
+++ voidrig/Assets/Script/Player/MouseMovement.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class MouseMovement: MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+
+    float xRotate = 0f;
+   
+    float yRotate = 0f;
+
+    public float minClamp = -90f;
+    public float maxClamp = 90f;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotate -= mouseY;
+        
+        xRotate = Mathf.Clamp(xRotate, minClamp, maxClamp);
+
+        yRotate -= mouseX;
+
+        transform.localRotation = Quaternion.Euler(xRotate, yRotate, 0f);
+    }
+}
---
+++ voidrig/Assets/Script/Player/PlayerMovement.cs
@@ -0,0 +1,62 @@
+using System;
+using UnityEngine;
+
+public class PlayerMovement: MonoBehaviour
+{
+
+    private CharacterController controller;
+
+    public float speed = 10f;
+    public float gravity = -9.81f;
+    public float jump = 3f;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    bool isGround;
+    bool isMoving;
+
+    private Vector3 lastPos = new Vector3(0f, 0f, 0f);
+
+    Vector3 velocity;
+    private void Start()
+    {
+        controller= GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        isGround = Physics.CheckSphere(groundCheck.position , groundDistance, groundMask);
+
+        if (isGround && velocity.y < 0 )
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGround == true)
+        {
+            Debug.Log("Hello");
+            velocity.y = Mathf.Sqrt(jump * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
+        if (lastPos != gameObject.transform.position && isGround == true) {
+            isMoving = true;
+        }
+        else isMoving = false;
+
+        lastPos = gameObject.transform.position;
+
+
+    }
+}
